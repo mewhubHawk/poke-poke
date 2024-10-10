2c21081f506612229eaa5ac2a3df296356f20d15
K <34939082+mewhubHawk@users.noreply.github.com>
2024-10-10 11:18:36 +0100

    upload previous coding work (2 years ago)

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+document.querySelector("button").addEventListener("click", function () {
+  myHeading.textContent = `Ouch! Stop poking me!`;
+});
+
+/*
+<img src="media/body.png" width="200">
+<img src="media/head.png" width="200">
+<img src="media/o_o.png" width="200">
+<img src="media/>c.png" width="200">
+*/
